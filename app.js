710e4239f85390d3a003d1985a95f8f9ce714278
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-06-20 22:12:29 +0300

    Update app.js
    cors update typo

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors(corsOptions));z
+app.use(cors(corsOptions));
 
 app.listen(process.env.PORT, async () => {
   console.info(`Server is running at http://localhost:${process.env.PORT}`);
